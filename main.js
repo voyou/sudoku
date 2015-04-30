7cc21d8bbac31ed927923d167419354d1241faf0
Voyou Désoevuré <web1@voyou.org>
2015-04-30 11:57:15 +0100

    Use the symbols from the matheme, rather than numbers (and specify a serif font so they look better).

--- main.js
+++ main.js
@@ -3,8 +3,10 @@
 var SV = require('./sudoku-view');
 SV.init(jQuery);
 
+var t = ['S̷', 'S₁', 'S₂', '𝑎'];
+
 $(function() {
-  $('#main').sudoku(9);  
+  $('#main').sudoku(4, function(i) { return t[i-1]; });  
 });
 
 
